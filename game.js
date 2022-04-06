f2aed77dfcf042581f6314d4ddf2ec9da25ab6d7
jgotti1 <jgotti1@optonline.net>
2022-04-06 12:02:23 -0400

    added music and sound affects

--- game.js
+++ game.js
@@ -10,12 +10,14 @@ let body = document.querySelector("body");
 let lowtime = document.querySelector(".timer");
 let button = document.querySelector(".ctc");
 let speed;
+let bkmusic = document.getElementById("bkmusic");
+let monkeyYum = document.getElementById("monkeyYum");
 
+//set amount of minutes to play game//
 function restart() {
-  console.log("hello");
-  startingMin = prompt("How long do you want to play, ebter 1 ,2 ,3 minutes");
+  startingMin = prompt("How long do you want to play, enter 1 ,2 or 3 minutes");
   if (startingMin == 1 || startingMin == 2 || startingMin == 3) {
-    time = startingMin * 60;
+    time = 0.3 * 60;
     console.log(time);
     game();
     button.style.display = "none";
@@ -24,9 +26,32 @@ function restart() {
     stopGame();
   }
 }
-
+//background music//
+function musicPlay() {
+  bkmusic.play();
+  bkmusic.volume = 1.0;
+}
+function musicStop() {
+  bkmusic.pause();
+  bkmusic.volume = 0.0;
+  monkeyYum.pause();
+  monkeyYum.volume = 0.0;
+}
+//monkey Yum click sound/
+function monkeyYumPlay() {
+  monkeyYum.play();
+  monkeyYum.volume = 1.0;
+  bkmusic.volume = 0.25;
+}
+function monkeyYumOff() {
+  monkeyYum.play();
+  monkeyYum.volume = 0;
+  bkmusic.volume = 1.0;
+}
 //create monkey div//
 function game() {
+  musicPlay();
+  //setup div for moving monkey//
   let div = document.createElement("div");
   div.classList.add("monkey");
   playArea.appendChild(div);
@@ -40,11 +65,15 @@ function game() {
     div.style.backgroundImage = "url(assets/spinMonkeyx.gif)";
     score = score + 1;
     messageArea[0].innerText = score;
+    monkeyYumPlay();
   });
   div.addEventListener("mouseleave", function () {
     div.style.backgroundImage = "url(assets/spinMonkey.gif)";
     div.style.height = 215 + "px";
     div.style.width = 215 + "px";
+    setTimeout(() => {
+      monkeyYumOff();
+    }, 1500);
   });
   //count down clock//
   setInterval(updateCountDown, 1000);
@@ -59,39 +88,24 @@ function game() {
       lowtime.style.color = "red";
       if (time <= 0) {
         time = 0;
+        document.getElementById("gameOver").play();
         stopGame();
       }
     }
-    //stop game when time expires//
-    // function stopGame() {
-    //   playArea.style.backgroundImage = "none";
-    //   playArea.style.cursor = "none";
-    //   body.style.backgroundImage = "url(assets/gameover.jpg)";
-    //   div.style.backgroundImage = "none";
-    //   body.style.backgroundPosition = "left";
-    //   button.innerText = "Game Over Click her to play again ... 🐵🐵🐵";
-    //   button.style.border = "3px dashed red";
-    //   button.style.display = "block";
-    //   button.addEventListener("click", function () {
-    //     location.reload();
-    //   });
-    // }
   }
 
   div.steps = Math.random() * 20;
   div.direction = Math.floor(Math.random() * 4);
   window.requestAnimationFrame(moveMonkey);
 }
-
-function newFunction() {
-  setTimeout("", 5000);
-}
-
+//stop game options/
 function stopGame() {
+  musicStop();
   playArea.style.backgroundImage = "none";
   playArea.style.cursor = "none";
   body.style.backgroundImage = "url(assets/gameover.jpg)";
-  // div.style.backgroundImage = "none";
+  let monkey = document.querySelector(".monkey");
+  monkey.style.backgroundImage = "none";
   body.style.backgroundPosition = "left";
   button.innerText = "Game Over Click her to play again ... 🐵🐵🐵";
   button.style.border = "3px dashed red";
@@ -99,6 +113,11 @@ function stopGame() {
   button.addEventListener("click", function () {
     location.reload();
   });
+  setTimeout(() => {
+    let gameOver = document.getElementById("gameOver");
+    gameOver.pause();
+    gameOver.volume = 0.0;
+  }, 3000);
 }
 
 function moveMonkey() {
